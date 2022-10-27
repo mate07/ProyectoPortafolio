6e91a05ff75a59a84658ef463102cbbe645752ee
mate07 <echo.delta.vladimir@gmail.com>
2022-10-27 13:01:05 -0600

    adiccionado proyecto parte 2, portafolio asp.net core

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoPortafolio.Models;
+using System.Diagnostics;
+
+namespace ProyectoPortafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //Bloque Información
+            var p = new Personsa()
+            {
+                Nombre = "Vladimir Mate Villanueva",
+                Edad = 30,
+                Profesion = "Ingeniero en CC de la Computación",
+                lugarUni = "UNAB"
+            };
+
+            return View(p);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Personsa.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoPortafolio.Models
+{
+    public class Personsa
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Profesion { get; set; }
+        public string lugarUni { get; set; }
+    }
+}
